f38bf917ea1a7037c15acb40cd688ae13b0e2513
korayv <koray.vatan@agu.edu.tr>
2024-08-22 08:31:09 +0300

    Commenting

--- src/logParser.js
+++ src/logParser.js
@@ -1,28 +1,35 @@
 import logSet from "./log_set";
-
+/*
+**parseLogAndGenerateSequence is accepting logContent for splitting every line
+**The lines of logContent will be visited using for each and search the logEntry
+*/
 export const parseLogAndGenerateSequence = (logContent) => {
   const lines = logContent.split("\n");
   let sequenceDiagramText = "";
-
-  lines.forEach((line) => {
+  //forEach iterates over every line on the log file to find the logline written in json
+  lines.forEach((line) => { //o(n)
     logSet.forEach((logEntry) => {
       const logLineRegex = new RegExp(logEntry.logline[0], "g");
-
+      /*
+      **Checks the logLineRegex from the logSet is not null
+      */
       if (logLineRegex.exec(line) !== null) {
-        const rgxDynamicKey = /(\w+)\s?:\s?([^\s,]+)/g;
-        const rgxPlaceholder = /\{\{(.*?)\}\}/g;
+        const rgxPlaceholder = /\{\{(.*?)\}\}/g; //regex that takes the placeholder between the curly braces
+        const rgxDynamicKey = /(\w+)\s?:\s?([^\s,]+)/g; //regex key for with the dynamic approach after the curly  
 
         const extractDataFromLogLine = (line) => {
-          const keyValuePairs = {};
+          const keyValuePairs = {}; //Object that stores key value pairs  
           let match;
+          //Iterate until a match on the the dynamic key regex in the log line 
           while ((match = rgxDynamicKey.exec(line)) !== null) {
+            //Assigning the captured match[2] to assign the capturedValue match[2] in the keyValuePairs
             keyValuePairs[match[1]] = match[2];
           }
-          return keyValuePairs;
+          return keyValuePairs; //return the object contains the key-value pairs
         };
-
-        const logData = extractDataFromLogLine(line);
-        const updatedSequenceNote = logEntry.sequenceNote.replace(
+        const logData = extractDataFromLogLine(line); //Extracted data from the logLine
+        //sequence note is updated and replaced  with the rgxPlaceHolder with the matched item
+        const updatedSequenceNote = logEntry.sequenceNote.replace( 
           rgxPlaceholder,
           (match, key) => {
             return logData[key] ? logData[key] : `No Result for ${key}`;
--- src/log_set.js
+++ src/log_set.js
@@ -3,17 +3,17 @@ const logSet = [
     // logline: "/P2PCallManager handleInitiate/g",
     logline: "/P2PCallManager\\s+handl[A-Za-z]+/g",
     sequenceNote:
-      "XMPP -> BiP Client A: handleInitiate(from: {{from}}, callId : {{callId}})",
+      "XMPP -> BiP Client A: handleInitiate(from: {{from}})",
   },
-];
-// {
-//   logline: "P2PCallEventManager sendInfo",
-//   sequenceNote: "BiP Client A -> XMPP: event(Conferencejoined)",
-// },
-// {
+//   {
+//     logline: "P2PCallEventManager sendInfo",
+//     sequenceNote: "BiP Client A -> XMPP: event(Conferencejoined)",
+//   },
+//   {
 //   logline: "CallSetupTimeHelper Call",
 //   sequenceNote: "Note over BiP Client A: Call Setup",
 // },
+];
 // {
 //   logline: "JitsiActivity",
 //   sequenceNote: "jjj",
