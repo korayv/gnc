7ed89fb7bfab526ffbcec5e90f16b4e461ed9851
korayv <koray.vatan@agu.edu.tr>
2024-08-23 11:36:06 +0300

    time stamped on the arrow

--- src/logParser.js
+++ src/logParser.js
@@ -6,40 +6,42 @@ import logSet from "./log_set";
 export const parseLogAndGenerateSequence = (logContent) => {
   const lines = logContent.split("\n");
   let sequenceDiagramText = "";
+
   //forEach iterates over every line on the log file to find the logline written in json
   lines.forEach((line) => { //o(n)
     logSet.forEach((logEntry) => {
-      const logLineRegex = new RegExp(logEntry.logline[0], "g");
-      /*
-      **Checks the logLineRegex from the logSet is not null
-      */
-      if (logLineRegex.exec(line) !== null) {
+      // Modify to check if 'handleInitiate' is at the start of the log line
+      if (line.includes(logEntry.logline)) {
+        const dateTimeRegex = /\d{4}[-/]\d{2}[-/]\d{2} \d{2}:\d{2}:\d{2}[:.]\d{3}/g;
         const rgxPlaceholder = /\{\{(.*?)\}\}/g; //regex that takes the placeholder between the curly braces
-        const rgxDynamicKey = /(\w+)\s?:\s?([^\s,]+)/g; //regex key for with the dynamic approach after the curly  
-
+        const rgxDynamicKey = /(\w+)\s?:\s?([^\s,]+)/g; //regex key for with the dynamic approach after the curly
+        // const timeMatch = line.match(timeStamp); 
         const extractDataFromLogLine = (line) => {
           const keyValuePairs = {}; //Object that stores key value pairs  
           let match;
+
           //Iterate until a match on the the dynamic key regex in the log line 
           while ((match = rgxDynamicKey.exec(line)) !== null) {
             //Assigning the captured match[2] to assign the capturedValue match[2] in the keyValuePairs
             keyValuePairs[match[1]] = match[2];
-          }
-          return keyValuePairs; //return the object contains the key-value pairs
+          };
+
+          return keyValuePairs;
         };
+
         const logData = extractDataFromLogLine(line); //Extracted data from the logLine
         //sequence note is updated and replaced  with the rgxPlaceHolder with the matched item
-        const updatedSequenceNote = logEntry.sequenceNote.replace( 
-          rgxPlaceholder,
-          (match, key) => {
+        const updatedSequenceNote = logEntry.sequenceNote
+          .replace(rgxPlaceholder, (match, key) => {
             return logData[key] ? logData[key] : `No Result for ${key}`;
           }
-        );
-
-        sequenceDiagramText += updatedSequenceNote + " \n";
+          );
+        sequenceDiagramText += updatedSequenceNote + " - " + dateTimeRegex.exec(line) + "\n";
+ 
       }
     });
   });
 
   return sequenceDiagramText;
 };
+
--- src/log_set.js
+++ src/log_set.js
@@ -1,18 +1,22 @@
 const logSet = [
   {
-    // logline: "/P2PCallManager handleInitiate/g",
-    logline: "/P2PCallManager\\s+handl[A-Za-z]+/g",
+    logline: "P2PCallManager handleInitiate",
+    //logline: "P2PCallManager\\s+handl[A-Za-z]+/g",
     sequenceNote:
-      "XMPP -> BiP Client A: handleInitiate(from: {{from}})",
+      "XMPP -> BiP Client A: handleInitiate(from: {{from}} callId: {{callId}})",
   },
-//   {
-//     logline: "P2PCallEventManager sendInfo",
-//     sequenceNote: "BiP Client A -> XMPP: event(Conferencejoined)",
-//   },
-//   {
-//   logline: "CallSetupTimeHelper Call",
-//   sequenceNote: "Note over BiP Client A: Call Setup",
-// },
+  //   {
+  //     logline: "P2PCallEventManager sendInfo",
+  //     sequenceNote: "BiP Client A -> XMPP: event(Conferencejoined)",
+  //   },
+  //   {
+  //   logline: "CallSetupTimeHelper Call",
+  //   sequenceNote: "Note over BiP Client A: Call Setup",
+  // },
+  // {
+  //   logline: "CallSetupTimeHelper Call setup time took:",
+  //   sequenceNote: "Note over BiP Client A: Call Setup ",
+  // },
 ];
 // {
 //   logline: "JitsiActivity",
